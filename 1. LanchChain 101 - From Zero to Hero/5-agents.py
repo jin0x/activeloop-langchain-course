cb1bf821e6e1fa821fc52e17bd1ea7f7bc32efd6
John Leskas <john.leskas@gmail.com>
2024-05-04 18:34:34 +0300

    chore: agents example

---
+++ 1. LanchChain 101 - From Zero to Hero/5-agents.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+from langchain.llms import OpenAI
+from langchain.agents import AgentType
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import Tool
+from langchain.utilities import GoogleSearchAPIWrapper
+
+# Initialize LLM
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
+
+# remember to set the environment variables
+# “GOOGLE_API_KEY” and “GOOGLE_CSE_ID” to be able to use
+# Google Search via API.
+search = GoogleSearchAPIWrapper()
+
+tools = [
+    Tool(
+        name="google-search",
+        func=search.run,
+        description="useful for when you need to search google to answer questions about current events"
+    )
+]
+
+agent = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True,
+    max_iterations=6
+)
+
+response = agent("What's the latest news about the Mars rover?")
+print(response['output'])
