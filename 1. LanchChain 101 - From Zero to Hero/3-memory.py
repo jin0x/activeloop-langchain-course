ca0c87561d838df14447ec022dedb8650d2b3999
John Leskas <john.leskas@gmail.com>
2024-05-04 16:42:50 +0300

    chore: memory

---
+++ 1. LanchChain 101 - From Zero to Hero/3-memory.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
+conversation = ConversationChain(
+    llm=llm,
+    verbose=True,
+    memory=ConversationBufferMemory()
+)
+
+# Start the conversation
+conversation.predict(input="Tell me about yourself.")
+
+# Continue the conversation
+conversation.predict(input="What do you do?")
+conversation.predict(input="How can you help me with data analysis?")
+
+# Display the convesation
+print(conversation)
