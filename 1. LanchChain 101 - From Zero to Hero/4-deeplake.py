2b2a9bda6a5fdca6a8b030a0f4caa7b3593fea65
John Leskas <john.leskas@gmail.com>
2024-05-04 18:01:04 +0300

    chore: first working example with simple query

--- 1. LanchChain 101 - From Zero to Hero/4-deeplake.py
+++ 1. LanchChain 101 - From Zero to Hero/4-deeplake.py
@@ -4,8 +4,10 @@ load_dotenv()
 import os
 
 from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import DeepLake
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
+# from langchain_text_splitters import CharacterTextSplitter
 from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.agents import AgentType, initialize_agent, Tool
@@ -22,21 +24,29 @@ texts = [
     "Napoleon Bonaparte was born in 15 August 1769",
     "Louis XIV was born in 5 September 1638"
 ]
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# docs = text_splitter.split_documents(texts)
 docs = text_splitter.create_documents(texts)
 
 # create Deep Lake dataset
 # TODO: use your organization id here. (by default, org id is your username)
 my_activeloop_org_id = os.environ["ACTIVELOOP_ORG_ID"]
 
-my_activeloop_dataset_name = "langchain_course_from_zero_to_hero"
+my_activeloop_dataset_name = "activeloop_langchain_course_from_zero_to_hero"
 dataset_path = f"hub://{my_activeloop_org_id}/{my_activeloop_dataset_name}"
-print(f"🟣🟣🟣🟣{dataset_path}")
 db = DeepLake(dataset_path=dataset_path, embedding=embeddings, overwrite=True)
 
 # add documents to our Deep Lake dataset
 db.add_documents(docs)
 
+
+query = "When was Napoleon born?"
+docs = db.similarity_search(query)
+
+print(docs[0].page_content)
+# print(dir(db))
+
+'''
 retrieval_qa = RetrievalQA.from_chain_type(
     llm=llm,
     chain_type="stuff",
@@ -61,3 +71,4 @@ agent = initialize_agent(
 
 response = agent.run("When was Napoleon born?")
 print(response)
+'''
