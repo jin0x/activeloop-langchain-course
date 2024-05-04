1d0a33046854408b113ccb8ec11820454ae1e8de
John Leskas <john.leskas@gmail.com>
2024-05-04 19:43:04 +0300

    chore: updates

--- 2. Large Language Models and LangChain/track_usage.py
+++ 2. Large Language Models and LangChain/track_usage.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
-llm = OpenAI(model_name="text-davinci-003", n=2, best_of=2)
+llm = OpenAI(model="gpt-3.5-turbo-instruct", n=2, best_of=2)
 
 with get_openai_callback() as cb:
     result = llm("Tell me a joke")
     cost = cb.total_cost
-    print("$",round(cost, 5)) 
+    print(result)
+    print("$",round(cost, 5))
