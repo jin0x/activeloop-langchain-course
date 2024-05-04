aa1c4a9f3e5a3f0a8e7f58c5ca3ed409de9d784f
John Leskas <john.leskas@gmail.com>
2024-05-04 23:16:44 +0300

    chore: updates

--- 2. Large Language Models and LangChain/9-gpt4all.py
+++ 2. Large Language Models and LangChain/9-gpt4all.py
@@ -3,19 +3,24 @@ load_dotenv()
 
 # Import modules
 from langchain.llms import GPT4All
-from langchain import PromptTemplate, LLMChain
-from langchain.callbacks.base import CallbackManager
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+model_path = 'the_path_to_your_local_model'
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+llm = GPT4All(model=model_path, callback_manager=callback_manager, verbose=True)
 
 template = """Question: {question}
 
 Answer: Let's think step by step."""
+
 prompt = PromptTemplate(template=template, input_variables=["question"])
 
-callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-llm = GPT4All(model="E:/Backup/Documents/Softwares/GPT4All/Models/ggml-wizardLM-7B.q4_2.bin", callback_manager=callback_manager, verbose=True)
 llm_chain = LLMChain(prompt=prompt, llm=llm)
+query = "What happens when it rains somewhere?"
 
-question = "What happens when it rains somewhere?"
-llm_chain.run(question)
+result = llm_chain.run(query)
+
+print(result)
