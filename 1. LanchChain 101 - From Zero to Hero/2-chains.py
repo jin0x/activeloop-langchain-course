4f702f0e19f4a280100728d6ce9c32f4318ba709
John Leskas <john.leskas@gmail.com>
2024-05-04 16:33:01 +0300

    chore: chains example

---
+++ 1. LanchChain 101 - From Zero to Hero/2-chains.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+
+
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.9)
+prompt=PromptTemplate(
+    input_variables=['product'],
+    template="What is a good name for a company that makes {product}?"
+)
+
+chain = LLMChain(llm=llm, prompt=prompt)
+
+print(chain.run('eco-friendly water bottles'))
