4641c6f6e58f992bfa771566843dedcc6cb20cfc
John Leskas <john.leskas@gmail.com>
2024-05-06 11:59:21 +0300

    chore: update Pydantic to v2

--- 3. Learning How to Prompt/news_summarizer_ output_parsers.py
+++ 3. Learning How to Prompt/news_summarizer_ output_parsers.py
@@ -1,5 +1,5 @@
 from langchain.output_parsers import PydanticOutputParser
-from pydantic import validator
+from pydantic import field_validator
 from pydantic import BaseModel, Field
 from typing import List
 from langchain.prompts import PromptTemplate
@@ -13,7 +13,8 @@ class ArticleSummary(BaseModel):
     summary: List[str] = Field(description="Bulleted list summary of the article")
 
     # validating whether the generated summary has at least three lines
-    @validator('summary', allow_reuse=True)
+    @field_validator('summary')
+    @classmethod
     def has_three_or_more_lines(cls, list_of_lines):
         if len(list_of_lines) < 3:
             raise ValueError("Generated summary has less than three bullet points!")
