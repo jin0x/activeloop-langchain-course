fdfa94e969bf3a3beb60e385e0d6c4fe16409751
John Leskas <john.leskas@gmail.com>
2024-05-06 21:11:34 +0300

    chore: update code

--- 5. Combining Components Together with Chains/6__Youtube_Summarizer.py
+++ 5. Combining Components Together with Chains/6__Youtube_Summarizer.py
@@ -2,17 +2,16 @@ import os
 from dotenv import load_dotenv
 import yt_dlp
 import whisper
+import textwrap
 from langchain import OpenAI, LLMChain
 from langchain.chains.mapreduce import MapReduceChain
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
-from langchain.chains.summarize import load_summarize_chain
-import textwrap
 
 # load API kets
-load_dotenv() 
+load_dotenv()
 
 # set API keys
 openai_api_key = os.environ['OPENAI_API_KEY']
@@ -39,12 +38,11 @@ model = whisper.load_model("base")
 result = model.transcribe("lecuninterview.mp4")
 print(result['text'])
 
-with open ('text.txt', 'w') as file:  
+with open ('text.txt', 'w') as file:
     file.write(result['text'])
-    
 
 # Summarization with LangChain
-llm = OpenAI(model_name="text-davinci-003", temperature=0)
+llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0)
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
@@ -60,6 +58,7 @@ chain = load_summarize_chain(llm, chain_type="map_reduce")
 
 output_summary = chain.run(docs)
 wrapped_text = textwrap.fill(output_summary, width=100)
+print( chain.llm_chain.prompt.template )
 print(wrapped_text)
 
 # The "stuff" approach
@@ -71,21 +70,24 @@ prompt_template = """Write a concise bullet point summary of the following:
 
 CONSCISE SUMMARY IN BULLET POINTS:"""
 
-BULLET_POINT_PROMPT = PromptTemplate(template=prompt_template, 
-                        input_variables=["text"])
+BULLET_POINT_PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
 
 
-# nitialized the summarization chain using the stuff as chain_type and the prompt above.
-chain = load_summarize_chain(llm, 
-                             chain_type="stuff", 
-                             prompt=BULLET_POINT_PROMPT)
+# Initialized the summarization chain using the stuff as chain_type and the prompt above.
+chain = load_summarize_chain(
+    llm,
+    chain_type="stuff",
+    prompt=BULLET_POINT_PROMPT
+)
 
 output_summary = chain.run(docs)
 
-wrapped_text = textwrap.fill(output_summary, 
-                             width=1000,
-                             break_long_words=False,
-                             replace_whitespace=False)
+wrapped_text = textwrap.fill(
+    output_summary,
+    width=1000,
+    break_long_words=False,
+    replace_whitespace=False
+)
 print(wrapped_text)
 
 # The 'refine' summarization chain is a method for generating more accurate and context-aware summaries.
