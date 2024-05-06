d50f5bc3909c72859896d3aecabb9119e5f043b2
John Leskas <john.leskas@gmail.com>
2024-05-06 19:42:44 +0300

    chore: update code

--- 5. Combining Components Together with Chains/1__LLMChain.py
+++ 5. Combining Components Together with Chains/1__LLMChain.py
@@ -1,9 +1,17 @@
-from langchain import PromptTemplate, OpenAI, LLMChain
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.chains import ConversationChain, LLMChain, SimpleSequentialChain
+from langchain.chains.base import Chain
+from langchain.prompts import PromptTemplate
+from langchain_openai import OpenAI
+from langchain.output_parsers import CommaSeparatedListOutputParser
+from langchain.memory import ConversationBufferMemory
 
 prompt_template = "What is a word to replace the following: {word}?"
 
 # Set the "OPENAI_API_KEY" environment variable before running following line.
-llm = OpenAI(model_name="text-davinci-003", temperature=0)
+llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0)
 
 llm_chain = LLMChain(
     llm=llm,
@@ -25,6 +33,7 @@ llm_chain.apply(input_list)
 # .generate() method
 llm_chain.generate(input_list)
 
+
 # .predict() method
 prompt_template = "Looking at the context of '{context}'. What is an appropriate word to replace the following: {word}?"
 
@@ -37,9 +46,7 @@ llm_chain.predict(word="fan", context="object")
 # or .run() method
 llm_chain.run(word="fan", context="object")
 
-#Parsers
-from langchain.output_parsers import CommaSeparatedListOutputParser
-
+## 1. Parsers
 output_parser = CommaSeparatedListOutputParser()
 template = """List all possible words as substitute for 'artificial' as comma separated."""
 
@@ -50,10 +57,7 @@ llm_chain = LLMChain(
 
 llm_chain.predict()
 
-# Conversational Chain (Memory)
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-
+## 2. Conversational Chain (Memory)
 output_parser = CommaSeparatedListOutputParser()
 conversation = ConversationChain(
     llm=llm,
@@ -64,12 +68,44 @@ conversation.predict(input="List all possible words as substitute for 'artificia
 
 conversation.predict(input="And the next 4?")
 
-# Sequential Chain
-from langchain.chains import SimpleSequentialChain
+## 3. Sequential Chain
+# poet
+poet_template: str = """You are an American poet, your job is to come up with\
+poems based on a given theme.
 
-overall_chain = SimpleSequentialChain(chains=[LLMchain_one, LLMchain_two])
+Here is the theme you have been asked to generate a poem on:
+{input}\
+"""
 
-# Debug
+poet_prompt_template: PromptTemplate = PromptTemplate(
+    input_variables=["input"], template=poet_template)
+
+# creating the poet chain
+poet_chain: LLMChain = LLMChain(
+    llm=llm, output_key="poem", prompt=poet_prompt_template)
+
+# critic
+critic_template: str = """You are a critic of poems, you are tasked\
+to inspect the themes of poems. Identify whether the poem includes romantic expressions or descriptions of nature.
+
+Your response should be in the following format, as a Python Dictionary.
+poem: this should be the poem you received
+Romantic_expressions: True or False
+Nature_descriptions: True or False
+
+Here is the poem submitted to you:
+{poem}\
+"""
+
+critic_prompt_template: PromptTemplate = PromptTemplate(
+    input_variables=["poem"], template=critic_template)
+
+# creating the critic chain
+critic_chain: LLMChain = LLMChain(
+    llm=llm, output_key="critic_verified", prompt=critic_prompt_template)
+
+
+## 4. Debug
 template = """List all possible words as substitute for 'artificial' as comma separated.
 
 Current conversation:
@@ -85,10 +121,7 @@ conversation = ConversationChain(
 
 conversation.predict(input="")
 
-# Custom Chain 
-from langchain.chains import LLMChain
-from langchain.chains.base import Chain
-
+##  5. Custom Chain
 from typing import Dict, List
 
 
@@ -110,7 +143,7 @@ class ConcatenateChain(Chain):
         output_1 = self.chain_1.run(inputs)
         output_2 = self.chain_2.run(inputs)
         return {'concat_output': output_1 + output_2}
-    
+
 prompt_1 = PromptTemplate(
     input_variables=["word"],
     template="What is the meaning of the following word '{word}'?",
