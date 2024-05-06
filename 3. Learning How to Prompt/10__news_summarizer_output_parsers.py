edc097bb864da04dd06e8fb900833db5d0fd432a
John Leskas <john.leskas@gmail.com>
2024-05-06 12:24:37 +0300

    chore: finalize news sumarizer output parser

--- 3. Learning How to Prompt/10__news_summarizer_output_parsers.py
+++ 3. Learning How to Prompt/10__news_summarizer_output_parsers.py
@@ -4,6 +4,8 @@ load_dotenv()
 from pydantic import field_validator
 from pydantic import BaseModel, Field
 from typing import List
+import requests
+from newspaper import Article
 from langchain.prompts import PromptTemplate
 from langchain.output_parsers import PydanticOutputParser
 from langchain_openai import OpenAI
@@ -20,6 +22,33 @@ class ArticleSummary(BaseModel):
         if len(list_of_lines) < 3:
             raise ValueError("Generated summary has less than three bullet points!")
         return list_of_lines
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+}
+
+article_url = "https://www.artificialintelligence-news.com/2022/01/25/meta-claims-new-ai-supercomputer-will-set-records/"
+
+session = requests.Session()
+
+try:
+    response = session.get(article_url, headers=headers, timeout=10)
+
+    if response.status_code == 200:
+        article = Article(article_url)
+        article.download()
+        article.parse()
+
+    #   print(f"Title: {article.title}")
+    #   print(f"Text: {article.text}")
+    else:
+        print(f"Failed to fetch article at {article_url}")
+except Exception as e:
+    print(f"Error occurred while fetching article at {article_url}: {e}")
+
+# we get the article data from the scraping part
+article_title = article.title
+article_text = article.text
 
 # set up output parser
 parser = PydanticOutputParser(pydantic_object=ArticleSummary)
@@ -50,11 +79,11 @@ prompt = PromptTemplate(
 formatted_prompt = prompt.format_prompt(article_title=article_title, article_text=article_text)
 
 # instantiate model class
-model = OpenAI(model_name="text-davinci-003", temperature=0.0)
+model = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.0)
 
 # Use the model to generate a summary
 output = model(formatted_prompt.to_string())
 
 # Parse the output into the Pydantic model
-parsed_output = parser.parse(output)
+parsed_output = parser.parse(output.split("\"]}")[0] + "\"]}")
 print(parsed_output)
